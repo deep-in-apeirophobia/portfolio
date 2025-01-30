6c616664278d256085162100c0037e8131febac0
Atrin Hojjat <atrin.hojjat@gmail.com>
2025-01-30 04:12:45 -0500

    Fix window dimensions

--- src/hooks/useWindowDimensions.tsx
+++ src/hooks/useWindowDimensions.tsx
@@ -2,15 +2,27 @@
 
 import { useEffect, useState } from "react";
 
-const getWindowDimensions = () => {
+type DimensionData = {
+	width: number,
+	height: number,
+	fake?: boolean
+}
+
+const getWindowDimensions = (): DimensionData => {
+	if (!window) return { width: 1000, height: 1000, fake: true }
 	const { innerWidth: width, innerHeight: height } = window;
 	return { width, height }
 }
 
 const useWindowDimensions = () => {
-	const [dimensions, setDimensions] = useState(getWindowDimensions())
+	const [dimensions, setDimensions] = useState<DimensionData>({ width: 1000, height: 1000, fake: true })
 
 	useEffect(() => {
+		setDimensions((d) => {
+			if (d.fake) 
+				return getWindowDimensions()
+			return d
+		})
 		const listener = () => {
 			setDimensions(getWindowDimensions());
 		}

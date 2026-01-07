7d7e1b36c53d7cf376b21fa9dee9e7144c15eb88
Atrin Hojjat <atrin.hojjat@gmail.com>
2026-01-07 07:43:38 -0500

    Add menu for right side

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,120 +1,123 @@
-'use client'
-import { motion, AnimatePresence } from "motion/react"
+"use client";
+import { motion, AnimatePresence } from "motion/react";
 import Link from "next/link";
-import { useState } from "react"
+import { useState } from "react";
 
-const Menu = () => {
-	const [open, setOpen] = useState(false);
-	return (
-		<motion.div 
-			layout
-			className="
-				absolute top-10 left-4 md:left-10 p-4
+const Menu = (props: { position?: "left" | "right" }) => {
+  const { position = "left" } = props;
+  const [open, setOpen] = useState(false);
+  return (
+    <motion.div
+      layout
+      className={`
+				absolute top-10 p-4
+				${position === "left" ? "left-4 md:left-10 flex-row" : "right-4 md:right-10 flex-row-reverse"}
 				rounded-full  border-solid bg-neutral-800  overflow-hidden
-				flex flex-row items-center z-50 "
-			initial="close"
-			animate={open ? 'open' : 'close'}
-		>
+				flex items-center z-50 `}
+      initial="close"
+      animate={open ? "open" : "close"}
+    >
+      <button onClick={() => setOpen((o) => !o)} className="group">
+        <motion.svg width="20" height="20" viewBox="0,0 40,40">
+          <motion.line
+            className="stroke-white stroke-[5px] group-hover:stroke-white/50 transition-all duration-300"
+            variants={{
+              open: {
+                x1: 4,
+                y1: 4,
+                x2: 36,
+                y2: 36,
+              },
+              close: {
+                x1: 0,
+                y1: 4,
+                x2: 40,
+                y2: 4,
+              },
+            }}
+          />
+          <motion.line
+            className="stroke-white stroke-[5px] group-hover:stroke-white/50 transition-all duration-300"
+            variants={{
+              open: {
+                x1: 4,
+                y1: 36,
+                x2: 36,
+                y2: 4,
+              },
+              close: {
+                x1: 0,
+                y1: 36,
+                x2: 40,
+                y2: 36,
+              },
+            }}
+          />
+          <motion.line
+            className="stroke-white stroke-[5px] group-hover:stroke-white/50 transition-all duration-300"
+            variants={{
+              open: {
+                x1: 20,
+                y1: 20,
+                x2: 20,
+                y2: 20,
+              },
+              close: {
+                x1: 0,
+                y1: 20,
+                x2: 40,
+                y2: 20,
+              },
+            }}
+          />
+        </motion.svg>
+      </button>
 
-			<button onClick={() => setOpen(o => !o)} className="group">
-				<motion.svg width="20" height="20" viewBox="0,0 40,40">
-					<motion.line
-						className="stroke-white stroke-[5px] group-hover:stroke-white/50 transition-all duration-300"
-						variants={{
-							open: {
-								x1: 4,
-								y1: 4,
-								x2: 36,
-								y2: 36,
-							},
-							close: {
-								x1: 0,
-								y1: 4,
-								x2: 40,
-								y2: 4,
-							},
-						}}
-					/>
-					<motion.line
-						className="stroke-white stroke-[5px] group-hover:stroke-white/50 transition-all duration-300"
-						variants={{
-							open: {
-								x1: 4,
-								y1: 36,
-								x2: 36,
-								y2: 4,
-							},
-							close: {
-								x1: 0,
-								y1: 36,
-								x2: 40,
-								y2: 36,
-							},
-						}}
-					/>
-					<motion.line
-						className="stroke-white stroke-[5px] group-hover:stroke-white/50 transition-all duration-300"
-						variants={{
-							open: {
-								x1: 20,
-								y1: 20,
-								x2: 20,
-								y2: 20,
-							},
-							close: {
-								x1: 0,
-								y1: 20,
-								x2: 40,
-								y2: 20,
-							},
-						}}
-					/>
-				</motion.svg>
-			</button>
-
-			<AnimatePresence >
-				{open && (
-					<motion.div 
-						initial={{ width: 0}}
-						animate={{ width: '288px'}}
-						exit={{ width: 0}}
-						className="flex gap-8 px-8"
-					>
-						<motion.div
-							className="text-nowrap"
-							initial={{ opacity: 0, x: -30, y: -5 }}
-							animate={{ opacity: 1, x: 0, y: 0 }}
-							exit={{ opacity: 0, x: 10, y: 5 }}
-						>
-							<Link href="/">
-								Home
-							</Link>
-						</motion.div>
-						<motion.div
-							className="text-nowrap"
-							initial={{ opacity: 0, x: -30, y: -5 }}
-							animate={{ opacity: 1, x: 0, y: 0 }}
-							exit={{ opacity: 0, x: 10, y: 5 }}
-						>
-							<Link href="/contact-me">
-								Contact Me
-							</Link>
-						</motion.div>
-						<motion.div
-							className="text-nowrap"
-							initial={{ opacity: 0, x: -30, y: -5 }}
-							animate={{ opacity: 1, x: 0, y: 0 }}
-							exit={{ opacity: 0, x: 10, y: 5 }}
-						>
-							<Link href="/about">
-								About
-							</Link>
-						</motion.div>
-					</motion.div>
-				)}
-			</AnimatePresence>
-		</motion.div>
-	)
-}
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            initial={{ width: 0 }}
+            animate={{ width: "326px" }}
+            exit={{ width: 0 }}
+            className="flex items-center gap-8 px-4"
+          >
+            <motion.div
+              className="text-nowrap"
+              initial={{ opacity: 0, x: -30, y: -5 }}
+              animate={{ opacity: 1, x: 0, y: 0 }}
+              exit={{ opacity: 0, x: 10, y: 5 }}
+            >
+              <Link href="/">Home</Link>
+            </motion.div>
+            <motion.div
+              className="text-nowrap"
+              initial={{ opacity: 0, x: -30, y: -5 }}
+              animate={{ opacity: 1, x: 0, y: 0 }}
+              exit={{ opacity: 0, x: 10, y: 5 }}
+            >
+              <Link href="/blog">Blog</Link>
+            </motion.div>
+            <motion.div
+              className="text-nowrap"
+              initial={{ opacity: 0, x: -30, y: -5 }}
+              animate={{ opacity: 1, x: 0, y: 0 }}
+              exit={{ opacity: 0, x: 10, y: 5 }}
+            >
+              <Link href="/contact-me">Contact Me</Link>
+            </motion.div>
+            <motion.div
+              className="text-nowrap"
+              initial={{ opacity: 0, x: -30, y: -5 }}
+              animate={{ opacity: 1, x: 0, y: 0 }}
+              exit={{ opacity: 0, x: 10, y: 5 }}
+            >
+              <Link href="/about">About</Link>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </motion.div>
+  );
+};
 
 export default Menu;
--- src/hooks/useFollowPointer.tsx
+++ src/hooks/useFollowPointer.tsx
@@ -1,44 +1,47 @@
-"use client"
+"use client";
 
-import { frame } from "motion"
-import { useMotionValue, useScroll, useSpring } from "motion/react"
-import { RefObject, useEffect } from "react"
+import { frame } from "motion";
+import { useMotionValue, useScroll, useSpring } from "motion/react";
+import { RefObject, useEffect } from "react";
 
-const spring = { damping: 30, stiffness: 90, restDelta: 0.001 }
+const spring = { damping: 30, stiffness: 90, restDelta: 0.001 };
 
-export function useFollowPointer(ref: RefObject<HTMLDivElement | null>, bgRef: RefObject<HTMLDivElement | null>) {
-	const xPoint = useMotionValue(0)
-	const yPoint = useMotionValue(0)
-	const x = useSpring(xPoint, spring)
-	const y = useSpring(yPoint, spring)
-	const { scrollY } = useScroll()
+export function useFollowPointer(
+  ref: RefObject<HTMLDivElement | null>,
+  bgRef: RefObject<HTMLDivElement | null>,
+) {
+  const xPoint = useMotionValue(0);
+  const yPoint = useMotionValue(0);
+  const x = useSpring(xPoint, spring);
+  const y = useSpring(yPoint, spring);
+  const { scrollY } = useScroll();
 
-	useEffect(() => {
-		if (!ref.current) return
+  useEffect(() => {
+    if (!ref.current) return;
 
-		const handlePointerMove = ({ clientX, clientY }: MouseEvent) => {
-				const element = ref.current
+    const handlePointerMove = ({ clientX, clientY }: MouseEvent) => {
+      const element = ref.current;
 
-				frame.read(() => {
-						if (!element || !bgRef.current) return ;
-						xPoint.set(
-								clientX - element.offsetLeft - element.offsetWidth / 2
-						)
-						yPoint.set(
-								Math.min(
-									clientY - element.offsetTop - element.offsetHeight / 2 + scrollY.get(),
-									bgRef.current!.getBoundingClientRect().height - element.offsetWidth / 2
-								),
-						)
-				})
-		}
+      frame.read(() => {
+        if (!element || !bgRef.current) return;
+        xPoint.set(clientX - element.offsetLeft - element.offsetWidth / 2);
+        yPoint.set(
+          Math.min(
+            clientY -
+              element.offsetTop -
+              element.offsetHeight / 2 +
+              scrollY.get(),
+            bgRef.current!.getBoundingClientRect().height -
+              element.offsetWidth / 2,
+          ),
+        );
+      });
+    };
 
-		window.addEventListener("pointermove", handlePointerMove)
+    window.addEventListener("pointermove", handlePointerMove);
 
-		return () =>
-				window.removeEventListener("pointermove", handlePointerMove)
-	}, [])
+    return () => window.removeEventListener("pointermove", handlePointerMove);
+  }, []);
 
-	return { x, y }
+  return { x, y };
 }
-

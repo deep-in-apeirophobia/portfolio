6f401f969125c7dcaccf03b3661a05e37bc3355f
Atrin Hojjat <atrin.hojjat@gmail.com>
2026-01-07 07:43:12 -0500

    Fix headerbg flashing

--- src/hooks/useWindowDimensions.tsx
+++ src/hooks/useWindowDimensions.tsx
@@ -1,40 +1,44 @@
-"use client"
+"use client";
 
 import { useEffect, useState } from "react";
 
 type DimensionData = {
-	width: number,
-	height: number,
-	fake?: boolean
-}
+  width: number;
+  height: number;
+  fake?: boolean;
+};
 
 const getWindowDimensions = (): DimensionData => {
-	if (!window) return { width: 1000, height: 1000, fake: true }
-	const { innerWidth: width, innerHeight: height } = window;
-	return { width, height }
-}
+  if (typeof window === "undefined")
+    return { width: 1000, height: 1000, fake: true };
+  const { innerWidth: width, innerHeight: height } = window;
+  return { width, height };
+};
 
 const useWindowDimensions = () => {
-	const [dimensions, setDimensions] = useState<DimensionData>({ width: 1000, height: 1000, fake: true })
+  const [dimensions, setDimensions] = useState<DimensionData>({
+    width: 1000,
+    height: 1000,
+    fake: true,
+  });
 
-	useEffect(() => {
-		setDimensions((d) => {
-			if (d.fake) 
-				return getWindowDimensions()
-			return d
-		})
-		const listener = () => {
-			setDimensions(getWindowDimensions());
-		}
+  useEffect(() => {
+    setDimensions((d) => {
+      if (d.fake) return getWindowDimensions();
+      return d;
+    });
+    const listener = () => {
+      setDimensions(getWindowDimensions());
+    };
 
-		window.addEventListener('resize', listener)
+    window.addEventListener("resize", listener);
 
-		return () => {
-			window.removeEventListener('resize', listener)
-		}
-	}, [])
+    return () => {
+      window.removeEventListener("resize", listener);
+    };
+  }, []);
 
-	return dimensions
-}
+  return dimensions;
+};
 
 export default useWindowDimensions;
